0092775e196252ca50083bf0fafa25c580b81d60
seungdeng17 <seungdeng17@gmail.com>
2020-08-30 22:22:33 +0900

    feat: users 페이지 Footer 컴포넌트 추가

---
+++ src/components/users/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+import LanguageSelectBox from "@/components/common/LanguageSelector/LanguageSelectBox";
+
+const FooterWrap = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 40px;
+  background-color: #3b3a3c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Text = styled.p`
+  color: #939597;
+  font-size: 13px;
+  a {
+    color: #939597;
+    text-decoration: none;
+  }
+`;
+
+const Test = styled.div`
+  position: absolute;
+  right: 10px;
+`;
+
+const Footer = () => {
+  return (
+    <FooterWrap>
+      <Text>
+        © Created by <a href="mailto:stlee@rsupport.com">Lee Seung Taek</a>
+      </Text>
+      <Test>
+        <LanguageSelectBox />
+      </Test>
+    </FooterWrap>
+  );
+};
+
+export default Footer;
--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 
 import GoogleLogoutButton from "./GoogleLogoutButton";
+import Footer from "./Footer";
+
+const UsersWrap = styled.div`
+  position: relative;
+`;
 
 const Users = () => {
-  return <GoogleLogoutButton />;
+  return (
+    <UsersWrap>
+      <GoogleLogoutButton />
+      <Footer />
+    </UsersWrap>
+  );
 };
 
 export default Users;
