bbf52e302decb0bf72efb499c485a080c5731b66
seungdeng17 <seungdeng17@gmail.com>
2020-08-31 16:37:45 +0900

    feat: 사용자 사진 정보 렌더링 기능 추가

--- src/components/menus/header/UserInfo.jsx
+++ src/components/menus/header/UserInfo.jsx
@@ -2,24 +2,33 @@ import React from "react";
 import styled from "styled-components";
 
 import balloonRed from "@assets/images/balloon-red.png";
+import noPicture from "@assets/images/no-picture.png";
 
 const UserInfoWrap = styled.div`
   position: absolute;
   right: 0;
+  display: flex;
+  align-items: center;
 `;
-
-const BalloonWrap = styled.div``;
 
 const Balloon = styled.img`
   width: 20px;
-  height: 25px;
+  height: 23px;
   margin-right: 5px;
   &.used {
     opacity: 0.25;
   }
   :last-child {
-    margin-right: 0;
+    margin-right: 20px;
   }
+`;
+
+const UserImg = styled.img`
+  width: 34px;
+  height: 34px;
+  background-color: #eee;
+  border-radius: 50%;
+  cursor: pointer;
 `;
 
 const UserInfo = () => {
@@ -30,12 +39,13 @@ const UserInfo = () => {
 
   for (let i = 0; i < BALLOON_COUNT; i++) {
     const className = i < BALLOON_COUNT - TEST_COUNT ? "used" : "";
-    balloons.push(<Balloon className={className} src={balloonRed} alt="balloon img" />);
+    balloons.push(<Balloon key={i} className={className} src={balloonRed} alt="balloon img" />);
   }
 
   return (
     <UserInfoWrap>
-      <BalloonWrap>{balloons}</BalloonWrap>
+      <div>{balloons}</div>
+      <UserImg src={noPicture} alt="user img" />
     </UserInfoWrap>
   );
 };
