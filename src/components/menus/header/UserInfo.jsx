2ae4448193dc622c2c6d28ead5338b08a3d552cc
seungdeng17 <seungdeng17@gmail.com>
2020-08-31 15:50:46 +0900

    feat: 남은 풍선 표시 기능 추가

--- src/components/menus/header/UserInfo.jsx
+++ src/components/menus/header/UserInfo.jsx
@@ -1,13 +1,43 @@
 import React from "react";
 import styled from "styled-components";
 
+import balloonRed from "@assets/images/balloon-red.png";
+
 const UserInfoWrap = styled.div`
   position: absolute;
   right: 0;
 `;
 
+const BalloonWrap = styled.div``;
+
+const Balloon = styled.img`
+  width: 20px;
+  height: 25px;
+  margin-right: 5px;
+  &.used {
+    opacity: 0.25;
+  }
+  :last-child {
+    margin-right: 0;
+  }
+`;
+
 const UserInfo = () => {
-  return <UserInfoWrap>User Infomation</UserInfoWrap>;
+  const BALLOON_COUNT = 7;
+  const TEST_COUNT = 5;
+
+  let balloons = [];
+
+  for (let i = 0; i < BALLOON_COUNT; i++) {
+    const className = i < BALLOON_COUNT - TEST_COUNT ? "used" : "";
+    balloons.push(<Balloon className={className} src={balloonRed} alt="balloon img" />);
+  }
+
+  return (
+    <UserInfoWrap>
+      <BalloonWrap>{balloons}</BalloonWrap>
+    </UserInfoWrap>
+  );
 };
 
 export default UserInfo;
