318cf5d8e67a5ddd6814b137e79c5e9aefc31eb8
seungdeng17 <seungdeng17@gmail.com>
2020-09-07 19:54:31 +0900

    feat: 칭찬대상자 검색 기능 추가

--- src/components/pages/users/userList/PartList.jsx
+++ src/components/pages/users/userList/PartList.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { partNameLangData } from "@data/languages/part-team-name";
 
 import TeamList from "./TeamList";
 
 const PartListWrap = styled.li`
-  border-bottom: 1px dashed #e6e6e6;
+  display: ${({ show }) => (show ? "block" : "none")};
+  /* background-color: ${({ show }) => (show ? "red" : "green")}; */
+  border-bottom: ${({ show }) => (show ? "1px dashed #e6e6e6" : "none")};
   margin-bottom: 40px;
   h3 {
     font-size: 16px;
@@ -17,12 +19,14 @@ const PartListWrap = styled.li`
 `;
 
 const PartList = ({ language, langData, partName, teamsData }) => {
+  const [isShow, setShow] = useState(true);
+
   const part = partNameLangData[partName] ? partNameLangData[partName][language] : partName;
 
   return (
-    <PartListWrap>
+    <PartListWrap show={isShow}>
       <h3 id={part}>{part}</h3>
-      <TeamList {...{ language, langData, teamsData }} />
+      <TeamList {...{ language, langData, teamsData, setShow }} />
     </PartListWrap>
   );
 };
--- src/components/pages/users/userList/TeamList.jsx
+++ src/components/pages/users/userList/TeamList.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 import Team from "./Team";
 
-const TeamList = ({ language, langData, teamsData }) => {
+const TeamList = ({ language, langData, teamsData, setShow }) => {
   const teamList = [];
 
   for (let [teamName, members] of Object.entries(teamsData)) {
-    teamList.push(<Team key={teamName} {...{ language, langData, teamName, members }} />);
+    teamList.push(<Team key={teamName} {...{ language, langData, teamName, members, setShow }} />);
   }
 
   return <>{teamList}</>;
