497dbe9a8c3e3b7c15ed7f3670f4b802d1995ccc
seungdeng17 <seungdeng17@gmail.com>
2020-09-10 17:12:54 +0900

    modify: 공통 모달 컴포넌트 구조 수정

--- src/components/modals/praise/PraiseModal.jsx
+++ src/components/modals/praise/PraiseModal.jsx
@@ -7,28 +7,30 @@ import ModalContainer from "@components/modals/ModalContainer";
 import PraiseModalBody from "./PraiseModalBody";
 
 const PraiseModal = ({ isOpen, setOpen }) => {
+  // Modal Body Props
   const [message, setMessage] = useState("");
-  const { register, handleSubmit } = useForm();
   const {
     language: { langData, language },
     receiver: { name, englishName },
   } = useSelector((index) => index);
 
+  const handleChange = ({ target: { value } }) => setMessage(value);
+  const receiverName = language === LANGUAGES.KO ? name : englishName ? englishName : name;
+
+  // Essential Modal Props
+  const { register, handleSubmit } = useForm();
   const handleModalClose = () => {
     if (message === "") return setOpen(false);
     if (window.confirm(langData["T0001"])) return setOpen(false);
   };
 
-  const handleChange = ({ target: { value } }) => setMessage(value);
-
   const onSubmit = (data) => console.log(data);
-
-  const receiverName = language === LANGUAGES.KO ? name : englishName ? englishName : name;
 
   const modalTitleText = langData["L0022"];
   const submitBtnText = langData["L0027"];
   const closeBtnText = langData["L0028"];
 
+  // Modal Body
   const body = <PraiseModalBody {...{ receiverName, register, langData, handleChange }} />;
 
   return <ModalContainer {...{ isOpen, body, handleSubmit, handleModalClose, onSubmit, modalTitleText, submitBtnText, closeBtnText }} />;
