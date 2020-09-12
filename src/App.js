e9895537abae7b74063986ce1d17b1c659944e1f
seungdeng17 <seungdeng17@gmail.com>
2020-09-12 18:11:09 +0900

    modify: 컴포넌트 import 컨벤션에 맞게 수정

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Footer from "@components/menus/footer/Footer";
 import Login from "@components/pages/login/Login";
 import Users from "@components/pages/users/Users";
 import Congrats from "@components/pages/congrats/Congrats";
-import box from "@components/pages/box/Box";
+import Box from "@components/pages/box/Box";
 import Stats from "@components/pages/stats/Stats";
 import Guide from "@components/pages/guide/Guide";
 
@@ -43,7 +43,7 @@ const App = () => {
         <Route exact path="/" component={Login} />
         <Route path="/users" component={Users} />
         <Route path="/congrats" component={Congrats} />
-        <Route path="/box" component={box} />
+        <Route path="/box" component={Box} />
         <Route path="/stats" component={Stats} />
         <Route path="/guide" component={Guide} />
         <Redirect to="/" />
