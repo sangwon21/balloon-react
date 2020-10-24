08011512591a6ffec8c75847c30032aefecf7adf
seungdeng17 <seungdeng17@gmail.com>
2020-10-24 23:12:08 +0900

    style: 연락처 모달 스타일 레이아웃

--- src/components/modals/userInfo/UserInfoModalBody.jsx
+++ src/components/modals/userInfo/UserInfoModalBody.jsx
@@ -5,14 +5,17 @@ import { LANGUAGES } from "@constants/constant";
 import noPicture from "@assets/images/no-picture.png";
 
 const UserInfoModalBodyWrap = styled.div`
-  padding: 9px;
+  padding: 20px 30px;
+  display: flex;
+  font-size: 16px;
   img {
-    width: 120px;
-    height: 120px;
+    width: 140px;
+    height: 140px;
     border: 1px solid #e6e6e6;
     background-color: #eee;
     border-radius: 50%;
     box-sizing: border-box;
+    margin-right: 30px;
   }
 `;
 
@@ -21,11 +24,13 @@ const UserInfoModalBody = ({ language, receiver }) => {
   return (
     <UserInfoModalBodyWrap>
       <img src={receiver.picture || noPicture} alt="user img" />
-      <p>{name}</p>
-      <p>{receiver.team}</p>
-      <p>{receiver.tel}</p>
-      <p>{receiver.phone}</p>
-      <p>{receiver.email}</p>
+      <div>
+        <p>{name}</p>
+        <p>{receiver.team}</p>
+        <p>{receiver.tel}</p>
+        <p>{receiver.phone}</p>
+        <p>{receiver.email}</p>
+      </div>
     </UserInfoModalBodyWrap>
   );
 };
