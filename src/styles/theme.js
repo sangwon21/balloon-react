c41a0fabc804fc60b58d3ad80fb2d3dd63dbf9c7
seungdeng17 <seungdeng17@gmail.com>
2020-10-25 15:06:31 +0900

    style: 여름 테마 point color 추가

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,14 +1,15 @@
 const backgroundColor = {
-  // main: "#00a6de",
-  // mainRGB: "0, 166, 222",
-  // sub: "#1f8ecd",
-  // hover: "#70bbd9",
+  main: "#00a6de",
+  mainRGB: "0, 166, 222",
+  sub: "#1f8ecd",
+  hover: "#70bbd9",
+  point: "#1e90ff",
 
-  main: "#f1a9a0",
-  mainRGB: "241, 169, 160",
-  sub: "#ff9478",
-  hover: "#e08283",
-  point: "#e44a54",
+  // main: "#f1a9a0",
+  // mainRGB: "241, 169, 160",
+  // sub: "#ff9478",
+  // hover: "#e08283",
+  // point: "#e44a54",
 };
 
 const theme = {
