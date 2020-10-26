12563e5217c3b8bed819261064b1197ab4a627fe
seungdeng17 <seungdeng17@gmail.com>
2020-10-26 14:38:05 +0900

    modify: 하이라이트 제거

--- src/components/pages/users/userList/UserListSkeleton.jsx
+++ src/components/pages/users/userList/UserListSkeleton.jsx
@@ -5,21 +5,12 @@ const UserListSkeletonWrap = styled.div`
   width: 100%;
   height: 100vh;
   margin: 20px;
-  background-image: linear-gradient(100deg, rgba(255, 255, 255, 0) 0, rgba(255, 255, 255, 0.5) 100%),
-    /* highlight */ linear-gradient(#eee 20px, transparent 0), linear-gradient(#eee 20px, transparent 0), linear-gradient(#eee 180px, transparent 0),
+  background-image: linear-gradient(#eee 20px, transparent 0), linear-gradient(#eee 20px, transparent 0), linear-gradient(#eee 180px, transparent 0),
     linear-gradient(#eee 180px, transparent 0), linear-gradient(#eee 180px, transparent 0), linear-gradient(#eee 180px, transparent 0),
     linear-gradient(#eee 180px, transparent 0), linear-gradient(#eee 180px, transparent 0), linear-gradient(#eee 180px, transparent 0);
   background-repeat: repeat-y;
-  background-size: 50px 280px, /* highlight */ 75px 280px, 175px 280px, 150px 280px, 150px 280px, 150px 280px, 150px 280px, 150px 280px, 150px 280px,
-    150px 280px;
-  background-position: 0 0, /* highlight */ 0 0, 0 30px, 0 60px, 160px 60px, 320px 60px, 480px 60px, 640px 60px, 800px 60px, 960px 60px;
-  animation: shine 1.5s infinite;
-  @keyframes shine {
-    to {
-      background-position: 100% 0, /* move highlight to right */ 0 0px, 0 30px, 0 60px, 160px 60px, 320px 60px, 480px 60px, 640px 60px, 800px 60px,
-        960px 60px;
-    }
-  }
+  background-size: 75px 280px, 175px 280px, 150px 280px, 150px 280px, 150px 280px, 150px 280px, 150px 280px, 150px 280px, 150px 280px;
+  background-position: 0 0, 0 30px, 0 60px, 160px 60px, 320px 60px, 480px 60px, 640px 60px, 800px 60px, 960px 60px;
 `;
 
 const UserListSkeleton = () => {
